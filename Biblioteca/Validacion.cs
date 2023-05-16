b8e7753e8a7ffb6f10912e04c2f0d3883734a701
jvelazquez <jonathanvgms@gmail.com>
2023-05-16 10:22:59 -0300

    Add libray project

---
+++ Biblioteca/Validacion.cs
@@ -0,0 +1,15 @@
+﻿namespace Biblioteca;
+public static class Validacion
+{
+    public static void ValidarRango(int valor, string mensajeError)
+    {
+        if (valor <= 0)
+            throw new Exception(mensajeError);
+    }
+
+    public static void ValidarCadena(string cadena, string mensajeError)
+    {
+        if (string.IsNullOrEmpty(cadena))
+            throw new Exception(mensajeError);
+    }
+}
